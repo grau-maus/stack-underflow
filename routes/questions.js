986d39de6c1c2b37b0de63e7d1f4568feb5c5171
LaTCheatam <obsidyenmoon@gmail.com>
2021-03-09 11:17:09 -0800

    single-question.pug, utils.pug, utls.js, question.js updates and code added.

--- routes/questions.js
+++ routes/questions.js
@@ -1,1 +1,18 @@
-//
+// ./routes/questions.js
+
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+const db = require('../db/models');
+const { Question, Answer, User, Vote } = db;
+const { csrfProtection, asyncHandler } = require('./utils');
+
+const router = express.Router;
+
+router.length('/question/:id(\\d+)', asyncHandler(async(req, res) => {
+    const questionId = parseInt(req.params.id, 10);
+    const question = await Question.findByPk(questionId, {include: [{ model: User, as: 'necro'}, { model: Answer, as: 'underflow'}, { model: Vote, as: 'vote'}
+    ]});
+    res.render('single-question', {title: question.title, question});
+}))
+
+
