4c01cd5684a93b67faf775ec71d422857faf5c58
Mimi Ke <mimi.g.ke@gmail.com>
2021-03-09 15:02:58 -0600

    csurf

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-//const { sequelize } = require('./db/models');
+const { sequelize } = require('./db/models');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const { sessionSecret } = require('./config');
 const questionsRouter = require('./routes/questions');
+
 
 const app = express();
 
@@ -26,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(restoreUser);
 app.use(indexRouter);    // any req to '/' we'll nav to indexRouter. we're being explicit.
 app.use(usersRouter);
-app.use(questionsRouter);
+//app.use(questionsRouter);
 
 // set up session middleware
 const store = new SequelizeStore({ db: sequelize });
